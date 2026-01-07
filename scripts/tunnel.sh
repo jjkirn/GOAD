f20d3f20febcdefb5ec5173283723705861d6d27
Jim Kirn <jim.kirn@outlook.com>
2026-01-06 19:46:12 -0600

    Updated tunnel.sh script

--- scripts/tunnel.sh
+++ scripts/tunnel.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-ssh -D 8080 -f -N -o ServerAliveInterval=60 -o ServerAliveCountMax=3 jim@192.168.1.110
-echo "Tunnel established on port 8080!"
-ss -tulnp
+ssh -D 9050 -L 5601:192.168.56.50:5601 -L 9200:192.168.56.50:9200 -f -N -o ServerAliveInterval=60 -o ServerAliveCountMax=3 jim@192.168.1.110
+echo "Tunnel established!"
+echo "- SOCKS proxy on port 9050"
+echo "- ELK Kibana forwarded to localhost:5601"
+echo "- ELK Elasticsearch forwarded to localhost:9200"
+ss -tulnp | grep -E '9050|5601|9200'
+
